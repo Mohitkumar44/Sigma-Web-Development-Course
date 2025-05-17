ec39b0f7387e1dcd15fdcf35ff89fae2c33d2136
Mohit Kumar <mohit.kumar162534@gmail.com>
2025-05-17 22:24:24 +0530

    JavaScript Start

---
+++ P25 JavaCript/script.js
@@ -0,0 +1,13 @@
+let name = "hellow mohit";
+let num = name+10;
+console.log(name);
+console.log(num);
+console.log(typeof name);
+let dict ={
+    book:"A object used to study",
+    pen: "An object used to write",
+    shirt: "An object used to wear",
+    cooler: "An object used to regulate the temperature of human body",
+    laptop:"A calculating device"
+}
+console.log(dict);

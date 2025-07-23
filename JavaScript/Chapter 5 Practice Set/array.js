efc82312d442d2b738206e182071786e1e3c8968
Mohit Kumar <mohit.kumar162534@gmail.com>
2025-07-23 15:13:36 +0530

    Chapter 5 Practice Set

---
+++ JavaScript/Chapter 5 Practice Set/array.js
@@ -0,0 +1,14 @@
+const arr = [ 34 , "mohit", 45.3, false];
+// console.log(arr);
+// console.log(arr[3]);
+// console.log(arr.length);
+// arr[0] = "aniket verma";
+// console.log(arr);
+// console.log(typeof arr);
+
+// console.log(arr.toString());    // do not change the original aray , return a new string
+// console.log(arr);
+const arr2 = [ 4,6,"hello","world"];
+console.log(arr2.join(arr));
+let b = arr2.pop();
+console.log(b);
---
+++ JavaScript/Chapter 5 Practice Set/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+"world"];
+// console.log(arr2.join(arr));
+// let b = arr2.pop();
+// console.log(b);

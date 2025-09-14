ecc48fb95d7b0bfd504626a1e584bf78f736fa6a
Mohit Kumar <mohit.kumar162534@gmail.com>
2025-09-15 04:30:01 +0530

    proto

---
+++ P35/script.js
@@ -0,0 +1,27 @@
+// let obj = {
+//     a:1,
+//     b:"Mohit"
+// }
+// console.log(obj)
+// let animal = {
+//     eats : true
+// let rabbit = {
+//     jumps : true
+// }
+// rabbit.__proto__ = animal;
+// // console.log(rabbit)
+
+class Animal{
+    constructor(name){
+        this.name = name;
+        console.log("Object Created...")
+    }
+    eats(){
+        console.log("Khaa rha hoon")
+    }
+    jumps(){
+        console.log("kood rha hu")
+    }
+}
+let a = new Animal("Bunny");
+console.log(a)
